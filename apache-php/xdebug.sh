1ff8501cf7e9d91b53847ad05ba641e828fdbfb4
Sébastien LAVENANT <sebastien@lavenant.me>
2016-04-03 12:16:07 +0200

    chore: remove apache-php image

--- apache-php/xdebug.sh
+++
@@ -1,7 +0,0 @@
-apt-get -y install php5-xdebug
-
-XDEBUG_SO="$(command find '/usr/lib/php5/' -name 'xdebug.so' | command head -n 1)"
-echo "; configuration for php XDebug module
-; priority=20
-zend_extension=${XDEBUG_SO}" > "/etc/php5/mods-available/xdebug.ini"
-
